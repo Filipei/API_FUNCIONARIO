2fbe5d5be5b1364c6df6c75e548fde9d62485ee1
Felipe Inácio <felipejjj92@gmail.com>
2026-07-03 18:10:47 -0300

    Subindo codigo fonte

---
+++ test/funcionario.test.js
@@ -0,0 +1,18 @@
+const request = require("supertest")
+
+const app = require("../app")
+
+describe("Api de funcionarios" , ()=>{
+    it("Deve cadastrar um funcionario" , async()=>{
+        const resposta = await request(app)
+        .post("funcionarios")
+        .send({
+            nome: "Maria",
+            cargo: "Analista"
+        })
+
+        espect(resposta.statusCode.toBe(201))
+        espect(resposta.body.nome).toBe("Maria")
+        espect(resposta.body.cargo).toBe("Analista")
+    })
+})
